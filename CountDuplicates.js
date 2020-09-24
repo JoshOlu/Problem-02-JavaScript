8ed69204d1b6eea6b570a3da6a3befbd12e42f06
Josh <jolumese@live.com>
2020-09-24 18:06:16 -0400

    CountDuplicates
    CountDUplicates Problem 2 Solved

--- CountDuplicates.js
+++ CountDuplicates.js
@@ -1,7 +1,21 @@
 class CountDuplicates {
 
     countDups(input){
-        return null;
+    	var dupeCount = 0
+    	var dict = {};
+    	for (var key = 0; key < input.length; key++) {
+    		if (dict.hasOwnProperty(input[key])) {
+    			dict[input[key]] += 1;
+    		} else {
+    			dict[input[key]] = 0;
+    		}
+    	}
+    	for (var i in dict) {
+    		if (dict[i] > 0) {
+    			dupeCount += 1;
+    		}
+    	}
+        return dupeCount;
     }
 }
 
